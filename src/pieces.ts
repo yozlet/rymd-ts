3c74cd1e1479d24faab350c7ac6df4efef8ed8ac
Yoz Grahame <yoz@yoz.com>
2025-01-18 23:09:12 -0800

    pieces: rejig class with less typing, fix modulo bug

--- src/pieces.ts
+++ src/pieces.ts
@@ -1,174 +1,194 @@
-// TODO: make this a class
-
-
-
-enum ShapeName {
-    i,
-    o,
-    j,
-    l,
-    t,
-    s,
-    z
-}
-enum Rotation {
-    zero,
-    one,
-    two,
-    three
-}
+export type PieceName = 'i' | 'o' | 'j' | 'l' | 't' | 's' | 'z';
 
 class Piece {
-    public shapeName: ShapeName;
-    public rotation: Rotation;
+    public pieceName: PieceName;
+    public rotation: number;
+    private static pieceNames: Array<PieceName> = ['i', 'o', 'j', 'l', 't', 's', 'z'];
 
-    constructor(shapeName: ShapeName, rotation: Rotation) {
-        this.shapeName = shapeName;
+    constructor(shapeName: PieceName, rotation: number) {
+        this.pieceName = shapeName;
         this.rotation = rotation;
     }
 
     public rotateRight(): void {
-        this.rotation = <Rotation>(this.rotation + 1) % pieceStructures[this.shapeName].length;
+        this.rotation = (this.rotation + 1) % this.pieceStructures[this.pieceName].length;
     }
 
     public rotateLeft(): void {
-        this.rotation = (this.rotation - 1) % pieceStructures[this.shapeName].length;
+        // the old Javascript negative modulo workaround
+        const l = this.pieceStructures[this.pieceName].length;
+        this.rotation = ((this.rotation - 1) + l) % l;
     }
 
     public getShape(): Array<Array<string>> {
-        return pieceStructures[this.shapeName][this.rotation];
+        return this.pieceStructures[this.pieceName][this.rotation];
     }
+
+    public getBoundingHandles(): { xMin: number, xMax: number, yMin: number, yMax: number, xHandle: number, yHandle: number } {
+        const shape = this.getShape();
+        let xMin: number = 3;
+        let yMin: number = 3;
+        let xMax: number = 0;
+        let yMax: number = 0;
+        let xHandle: number = 0;
+        let yHandle: number = 0;
+        for (let row = 0; row < shape.length; row++) {
+            for (let col = 0; col < shape[row].length; col++) {
+                if (shape[row][col] != ' ') {
+                    xMin = Math.min(xMin, col);
+                    yMin = Math.min(yMin, row);
+                    xMax = Math.max(xMax, col);
+                    yMax = Math.max(yMax, row);
+                    xHandle = col;
+                    yHandle = row;
+                }
+            }
+        }
+        return { xMin, xMax, yMin, yMax, xHandle, yHandle };
+    }
+
+    // Return a shuffled array of piece names  
+    public static shufflePieceNames(): Array<PieceName> {
+        const sequence = <Array<PieceName>>Object.values(this.pieceNames);
+        for (let i = sequence.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [sequence[i], sequence[j]] = [sequence[j], sequence[i]];
+        }
+        return sequence;
+    }
+
+    public pieceStructures: { [key in PieceName]: Array<Array<Array<string>>> } = {
+        ['i']: [
+            [
+                [' ', ' ', ' ', ' '],
+                ['i', 'i', 'i', 'i'],
+                [' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', 'i', ' ', ' '],
+                [' ', 'i', ' ', ' '],
+                [' ', 'i', ' ', ' '],
+                [' ', 'i', ' ', ' '],
+            ],
+        ],
+        ['o']: [
+            [
+                [' ', ' ', ' ', ' '],
+                [' ', 'o', 'o', ' '],
+                [' ', 'o', 'o', ' '],
+                [' ', ' ', ' ', ' '],
+            ]
+        ],
+        ['j']: [
+            [
+                [' ', ' ', ' ', ' '],
+                ['j', 'j', 'j', ' '],
+                [' ', ' ', 'j', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', 'j', ' ', ' '],
+                [' ', 'j', ' ', ' '],
+                ['j', 'j', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                ['j', ' ', ' ', ' '],
+                ['j', 'j', 'j', ' '],
+                [' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', 'j', 'j', ' '],
+                [' ', 'j', ' ', ' '],
+                [' ', 'j', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ]
+        ],
+        ['l']: [
+            [
+                [' ', ' ', ' ', ' '],
+                ['l', 'l', 'l', ' '],
+                ['l', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', 'l', ' ', ' '],
+                [' ', 'l', ' ', ' '],
+                [' ', 'l', 'l', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', ' ', 'l', ' '],
+                ['l', 'l', 'l', ' '],
+                [' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                ['l', 'l', ' ', ' '],
+                [' ', 'l', ' ', ' '],
+                [' ', 'l', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ]
+        ],
+        ['t']: [
+            [
+                [' ', ' ', ' ', ' '],
+                ['t', 't', 't', ' '],
+                [' ', 't', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', 't', ' ', ' '],
+                [' ', 't', 't', ' '],
+                [' ', 't', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', 't', ' ', ' '],
+                ['t', 't', 't', ' '],
+                [' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', 't', ' ', ' '],
+                ['t', 't', ' ', ' '],
+                [' ', 't', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ]
+        ],
+        ['s']: [
+            [
+                [' ', ' ', ' ', ' '],
+                [' ', 's', 's', ' '],
+                ['s', 's', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                ['s', ' ', ' ', ' '],
+                ['s', 's', ' ', ' '],
+                [' ', 's', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ]
+        ],
+        ['z']: [
+            [
+                [' ', ' ', ' ', ' '],
+                ['z', 'z', ' ', ' '],
+                [' ', 'z', 'z', ' '],
+                [' ', ' ', ' ', ' '],
+            ],
+            [
+                [' ', 'z', ' ', ' '],
+                ['z', 'z', ' ', ' '],
+                ['z', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' '],
+            ]
+        ]
+    };
+    
 }
 
-const pieceStructures: { [key in ShapeName]: Array<Array<Array<string>>> } = {
-    [ShapeName.i]: [
-        [
-            [' ', ' ', ' ', ' '],
-            ['i', 'i', 'i', 'i'],
-            [' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', 'i', ' ', ' '],
-            [' ', 'i', ' ', ' '],
-            [' ', 'i', ' ', ' '],
-            [' ', 'i', ' ', ' '],
-        ],
-    ],
-    [ShapeName.o]: [
-        [
-            [' ', ' ', ' ', ' '],
-            [' ', 'o', 'o', ' '],
-            [' ', 'o', 'o', ' '],
-            [' ', ' ', ' ', ' '],
-        ]
-    ],
-    [ShapeName.j]: [
-        [
-            [' ', ' ', ' ', ' '],
-            ['j', 'j', 'j', ' '],
-            [' ', ' ', 'j', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', 'j', ' ', ' '],
-            [' ', 'j', ' ', ' '],
-            ['j', 'j', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            ['j', ' ', ' ', ' '],
-            ['j', 'j', 'j', ' '],
-            [' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', 'j', 'j', ' '],
-            [' ', 'j', ' ', ' '],
-            [' ', 'j', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ]
-    ],
-    [ShapeName.l]: [
-        [
-            [' ', ' ', ' ', ' '],
-            ['l', 'l', 'l', ' '],
-            ['l', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', 'l', ' ', ' '],
-            [' ', 'l', ' ', ' '],
-            [' ', 'l', 'l', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', ' ', 'l', ' '],
-            ['l', 'l', 'l', ' '],
-            [' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            ['l', 'l', ' ', ' '],
-            [' ', 'l', ' ', ' '],
-            [' ', 'l', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ]
-    ],
-    [ShapeName.t]: [
-        [
-            [' ', ' ', ' ', ' '],
-            ['t', 't', 't', ' '],
-            [' ', 't', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', 't', ' ', ' '],
-            [' ', 't', 't', ' '],
-            [' ', 't', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', 't', ' ', ' '],
-            ['t', 't', 't', ' '],
-            [' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', 't', ' ', ' '],
-            ['t', 't', ' ', ' '],
-            [' ', 't', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ]
-    ],
-    [ShapeName.s]: [
-        [
-            [' ', ' ', ' ', ' '],
-            [' ', 's', 's', ' '],
-            ['s', 's', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            ['s', ' ', ' ', ' '],
-            ['s', 's', ' ', ' '],
-            [' ', 's', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ]
-    ],
-    [ShapeName.z]: [
-        [
-            [' ', ' ', ' ', ' '],
-            ['z', 'z', ' ', ' '],
-            [' ', 'z', 'z', ' '],
-            [' ', ' ', ' ', ' '],
-        ],
-        [
-            [' ', 'z', ' ', ' '],
-            ['z', 'z', ' ', ' '],
-            ['z', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' '],
-        ]
-    ]
-};
 
-export { Piece, ShapeName, Rotation };
+export { Piece };
--- src/world.ts
+++ src/world.ts
@@ -1,20 +1,30 @@
 import { Cell } from "./cell";
-import { Piece } from "./pieces";
+import { Piece, PieceName } from "./pieces";
 
 class World {
     private grid: Array<Array<Cell>>;
-    private pieceSequence: Array<Piece>;
+
+    // List of upcoming pieces
+    private pieceSequence: Array<PieceName>;
+    private heldPiece: Piece;
 
     constructor(gridHeight: number, gridWidth: number) {
         this.grid = Array.from({ length: gridHeight }, 
                                 () => Array.from({ length: gridWidth }, 
                                                  () => new Cell()));
-        this.pieceSequence = [];
+        // Start piece sequence with a two shuffled arrays of piece names
+        this.pieceSequence = Piece.shufflePieceNames().concat(Piece.shufflePieceNames());
+        this.heldPiece = new Piece(this.pieceSequence[0], 0);
     }
 
     public getCell(x: number, y: number): Cell {
         return this.grid[y][x];
     }
+
+    public getHeldPiece(): Piece {
+        return this.heldPiece;
+    }
+
 }
 
 export { World };
