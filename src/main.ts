48ae98224ad5221f9a65f31aff601b3155aec903
Yoz Grahame <yoz@yoz.com>
2025-01-31 10:45:56 -0800

    main: TS fix for window.game

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './style.css';
 import { Game } from './game';
-
 const game = new Game();
+
+// Keep game object accessible from browser devtools
+declare global {
+  interface Window {
+    game: Game;
+  }
+}
 window.game = game;
+
 game.start();
