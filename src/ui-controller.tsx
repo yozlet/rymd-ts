c4d0ca08bebc9cefb062c48fdebc942459582d62
Yoz Grahame <yoz@yoz.com>
2025-02-08 21:17:44 -0800

    Preact signals now works for HTML UI

--- src/ui-controller.tsx
+++ src/ui-controller.tsx
@@ -1,63 +1,50 @@
-import { render, Component, ComponentChildren } from 'preact';
+import { render, JSX } from 'preact';
 import { Flavours } from './flavour';
 import { SignalBox } from './signalbox';
 import { InputRouter } from './inputrouter';
-
-interface UIState {
-}
-
-interface UIProps {
-}
+import { effect } from '@preact/signals';
 
 export class UIController {
     private container: HTMLElement;
     
     constructor(container: HTMLElement) {
         this.container = container;
+
+        effect(() => {
+            console.log(`Flavour: ${SignalBox.heldPieceFlavour.value.name}`);
+        });
     }
 
     render() {
         render(<UIComponent/>, this.container);
     }
 }
+ 
+function UIComponent(): JSX.Element {
+    return (
+        <div id="ui-container">
+            <FlavourList/>
+        </div>
+    );
+}   
 
-export class UIComponent extends Component<UIProps, UIState> {
-    //private statsContainer!: HTMLElement;
-    //private debugInfo!: HTMLElement;
 
-    constructor(props: UIProps) {
-        super(props);
-    }   
 
-    render(): ComponentChildren {
-        return (
-            <div id="ui-container">
-                {/* <div id="stats" ref={e => this.statsContainer = e!}></div> */}
-                <div id="flavors-container">
-                    <h3>Flavors</h3>
-                    <div className="flavors-list">
-                        {Object.values(Flavours).map((flavour) => (
-                            <button
-                                key={flavour.name}
-                                className={`flavor-btn ${SignalBox.heldPieceFlavour.value === flavour ? 'active' : ''}`}
-                                onClick={() => InputRouter.setFlavour(flavour)}
-                                style={{ backgroundColor: flavour.colour }}
-                            >
-                                {flavour.name}
-                            </button>
-                        ))}
-                    </div>
-                </div>
-                {/* <div id="debug-info" ref={e => this.debugInfo = e!}></div> */}
+function FlavourList(): JSX.Element {
+    return (
+        <div id="flavours-container">
+            <div className="flavours-list" id="flavours">
+                {Object.values(Flavours).map((flavour) => (
+                    <button
+                        key={flavour.name}
+                        className={`flavour ${SignalBox.heldPieceFlavour.value === flavour ? 'active' : ''}`}
+                        onClick={() => InputRouter.setFlavour(flavour)}
+                        style={{ color: flavour.colour }}
+                >
+                    {flavour.name}
+                </button>
+            ))}
             </div>
-        );
-    }   
-
-    // updateStats(fps: number): void {
-    //     this.statsContainer.innerHTML = `FPS: ${fps.toFixed(1)}`;
-    // }
-
-    // updateDebugInfo(info: string): void {
-    //     this.debugInfo.textContent = info;
-    // }
-} 
+        </div>
+    );
+}
